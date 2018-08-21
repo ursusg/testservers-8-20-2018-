5d608f37116d217c389d45fab0bb9ad421649263
Ursus <ugurungsus@gmail.com>
2018-08-20 19:33:52 -0500

    added more JS

--- servers.js
+++ servers.js
@@ -1,13 +1,54 @@
 const http = require("http");
-const PORT = 7000;
+const PORT1 = 7000;
+const PORT2 = 7500;
 
-var handleRequest = (req, res) => {
+// Array of good phrases
+var goodPhrases = [
+    "You are amazing",
+    "You have a great smile",
+    "Holy shit I've never met someone like you..",
+    "I hate anyone who hates you"
+];
 
-    res.end(`You are amazing`);
+// Array of bad phrases
+var badPhrases = [
+    "Who the hell are you?!?",
+    "Stop. Just stop trying.",
+    "Are you serious? Who do you think you are?",
+    "This is a private Christian server, please mind your language."
+];
+
+// Functions to grab a random index of array
+function randomGoodPhrase () {
+    var response = goodPhrases[Math.floor(Math.random() * goodPhrases.length)]
+    return response
+}
+function randomBadPhrase () {
+    var response = badPhrases[Math.floor(Math.random() * badPhrases.length)]
+    return response
+}
+
+// Handle request for Port 1
+var handleRequest1 = (req, res) => {
+    res.end(randomGoodPhrase());
 };
 
-var server = http.createServer(handleRequest);
+// Handle Request for port 2
+var handleRequest2 = (req, res) => {
 
-server.listen(PORT, () => {
-    console.log(`Server listening on: http://localhost:${PORT}`);
+    res.end(randomBadPhrase());
+};
+
+// Storing the HTTP create server for port 1
+var server1 = http.createServer(handleRequest1);
+var server2 = http.createServer(handleRequest2);
+
+// Server 1 using handleRequest1 on Port 1
+server1.listen(PORT1, () => {
+    console.log(`Server listening on: http://localhost:${PORT1}`);
+});
+
+// Server 2 using handleRequest2 on Port 2
+server2.listen(PORT2, () => {
+    console.log(`Server listening on: http://localhost:${PORT2}`);
 });
